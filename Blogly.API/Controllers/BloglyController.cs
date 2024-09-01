d6a2f28afc0b85ac7d1955f184d7018dbc670330
Toyin Onagoruwa <onagoruwam@gmail.com>
2024-09-01 13:45:55 +0100

    checking the ruleset i added (#4)
    * want to see the ruleset in action
    
    * added another test
    
    * checking the ruleset

--- Blogly.API/Controllers/BloglyController.cs
+++ Blogly.API/Controllers/BloglyController.cs
@@ -14,7 +14,6 @@ public class BloglyController(IBloglyService bloglyService) : ControllerBase
     public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request, CancellationToken token)
     {
         var response = await bloglyService.CreateNewUserAsync(request, token);
-
         if (response == null) return BadRequest();
         return CreatedAtAction("GetUser", new {id = response.Id}, response);
     }
@@ -30,7 +29,6 @@ public class BloglyController(IBloglyService bloglyService) : ControllerBase
     public async Task<IActionResult> GetUser([FromRoute] Guid id, CancellationToken token)
     {
         var response = await bloglyService.GetUserAsync(id, token);
-
         if (response is null) return NotFound();
         return Ok(response);
     }
