bfc1fdbe25c379aba352eddccb628474dc20b88c
toyin onagoruwa <onagoruwam@gmail.com>
2024-09-01 14:09:49 +0100

    added a failing test

---
+++ Blogly.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace Blogly.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // arrange
+        int firstNum = 2;
+        int secondNum = 3;
+        
+        // act
+        int sum = firstNum + secondNum;
+        
+        // assert
+        Assert.Equal(4, sum);
+    }
+}
